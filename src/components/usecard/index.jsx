3cd78c0c655fcc3c353248413cf950bcc889866b
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-04-07 19:08:54 -0300

    prototipo

--- src/components/usecard/index.jsx
+++ src/components/usecard/index.jsx
@@ -1,21 +1,26 @@
 import card from '../../../public/card.json'
+import './styles.css'
 
 export default function Teste() {
   return (
     <div>
-      <h1>PRO</h1>
+      <div className="pro">
+        <h1>PRO</h1>
+      </div>
 
       <img src={card.photo} alt="" />
-      <h2>O seu nome é {card.name}</h2>
+      <h2>{card.name}</h2>
       <h3>{card.city}</h3>
 
-      <p>{card.description}</p>
+      <h4>{card.description}</h4>
 
-      <button>Message</button>
-      <button>Following</button>
+      <button className='primary'>Message</button>
+      <button className='botao'>Following</button>
 
-      <h3>Skills</h3>
-      {card.skills.map(skill => <button key={skill}>{skill}</button>)}
+      <div className="skills">
+        <h4>Skills</h4>
+        {card.skills.map(skill => <button className='botao-secundario' key={skill}>{skill}</button>)}
+      </div>
     </div>
   )
 }
